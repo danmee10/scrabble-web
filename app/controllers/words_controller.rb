9b7ed2730e8679fe9f6c3a6d10b0301c79339260
Dan Mee <danmee10@gmail.com>
2013-06-11 09:27:10 -0600

    filed in index and create actions

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,16 @@
 class WordsController < ApplicationController
   def index
+    @new_word = Word.new
+    @previous_words = Word.all
+    @last_word = Word.all.last
   end
 
   def create
+    @new_word = Word.new(params[:word])
+    if @new_word.save
+      redirect_to "/words"
+    else
+      raise boom
+    end
   end
 end
